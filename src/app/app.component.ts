e24b2cbf8e462d8cad29d82f597c31389c32ddf4
TomcikKometa <tomcik48_84@o2.pl>
2024-08-30 22:28:30 +0200

    confugire sidebar and global css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InquiryHeaderComponent } from './features/inquiry-header/inquiry-header.component';
+import { SidebarComponent } from './features/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   providers:[],
-  imports: [RouterOutlet,InquiryHeaderComponent],
+  imports: [RouterOutlet,InquiryHeaderComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
