f31ad7e291b2085adc65bf610f43217629c7ef05
TomcikKometa <tomcik48_84@o2.pl>
2024-08-30 08:30:53 +0200

    develop header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InquiryHeaderComponent } from './features/inquiry-header/inquiry-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   providers:[],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,InquiryHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
