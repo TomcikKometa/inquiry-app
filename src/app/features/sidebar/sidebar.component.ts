2f930d1369ef6d38dff1253ef710100a29c9091d
TomcikKometa <tomcik48_84@o2.pl>
2024-09-03 15:26:37 +0200

    add hoover to components details

--- src/app/features/sidebar/sidebar.component.ts
+++ src/app/features/sidebar/sidebar.component.ts
@@ -1,19 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'inq-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   protected isUserDetail = false;
+  protected isUserDetailRow = false;
+  protected isInquiryDetail = false;
+  protected isInquiryDetailRow = false;
+  protected isComponentUserHoover = false;
+  protected isComponentInquiryHoover = false;
+  idFor: number = 0;
 
-  protected showItems(details: string) {
-    console.log(1);
-    if (details) {
+  userDatailList = [
+    { id: 1, text: 'bla bla' },
+    { id: 2, text: 'la la la' }
+  ];
+
+  inquiryDetailList = [
+    { id: 1, text: 'To fill' },
+    { id: 2, text: 'Done' },
+    { id: 3, text: 'Scores' }
+  ];
+
+  protected showItems(mainDetails: string, rowDetails?: string) {
+    console.log(mainDetails);
+    if (mainDetails === 'userDetail') {
+      this.isComponentInquiryHoover = false;
       this.isUserDetail = !this.isUserDetail;
+      this.isUserDetail == false ? (this.idFor = 0) : 0;
+      this.isComponentUserHoover = true;
     }
+
+    if (mainDetails === 'inquiryDetail') {
+      this.idFor = 0;
+      this.isComponentUserHoover = false;
+      this.isInquiryDetail = !this.isInquiryDetail;
+      this.isComponentInquiryHoover = true;
+      this.isUserDetail == false ? (this.idFor = 0) : 0;
+    }
+  }
+
+  id(id: number) {
+    this.idFor = id;
   }
 }
