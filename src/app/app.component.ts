8754284b73d58d52eca4d748d597bcde75de446a
TomcikKometa <tomcik48_84@o2.pl>
2024-09-04 13:38:38 +0200

    configure sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InquiryHeaderComponent } from './features/inquiry-header/inquiry-header.component';
@@ -7,10 +7,17 @@ import { SidebarComponent } from './features/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers:[],
-  imports: [RouterOutlet,InquiryHeaderComponent,SidebarComponent],
+  providers: [],
+  imports: [RouterOutlet, InquiryHeaderComponent, SidebarComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  protected isSidebar: boolean = true;
+
+  protected getSidenavEvent(event: boolean) {
+    if (event) {
+      this.isSidebar = !this.isSidebar;
+    }
+  }
 }
