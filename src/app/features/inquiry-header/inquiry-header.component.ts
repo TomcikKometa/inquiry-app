ab957a3e883996839a73e44c044126004f9b1bff
TomcikKometa <tomcik48_84@o2.pl>
2024-09-04 13:39:50 +0200

    add eventemitter

--- src/app/features/inquiry-header/inquiry-header.component.ts
+++ src/app/features/inquiry-header/inquiry-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'inq-inquiry-header',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './inquiry-header.component.css'
 })
 export class InquiryHeaderComponent {
+
+  @Output() public openSidebar:EventEmitter<boolean> = new EventEmitter<boolean>();
+opened : boolean = false
 
 }
