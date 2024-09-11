0ee6ef06f30d39c5b08a9da722e720a79b9b8572
TomcikKometa <tomcik48_84@o2.pl>
2024-09-11 09:12:00 +0200

    add r3egisterForm FormBuilder

--- src/app/features/shared-components/register/register.component.ts
+++ src/app/features/shared-components/register/register.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { RegisterFormName, RegisterFormService } from '../services/register-form/register.service';
 
 @Component({
   selector: 'inq-register',
   standalone: true,
-  imports: [MatCardModule, FormsModule,ReactiveFormsModule ],
+  imports: [MatCardModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  protected _registerForm!: FormGroup;
 
+  private readonly registerFormService: RegisterFormService = inject(RegisterFormService);
+
+  public ngOnInit(): void {
+    this._registerForm = this.registerFormService._registerForm;
+  }
+
+  protected get registerForm(): FormGroup {
+    return this._registerForm;
+  }
+
+  protected get registerFormName(): typeof RegisterFormName {
+    return RegisterFormName;
+  }
 }
