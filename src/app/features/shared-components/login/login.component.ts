edae3b7e7ec9df9581fa33e51aa334b44d6af977
TomcikKometa <tomcik48_84@o2.pl>
2024-10-01 10:05:06 +0200

    develop components

--- src/app/features/shared-components/login/login.component.ts
+++ src/app/features/shared-components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginFormService } from '../services/login-form/login-form.service';
-import { debounceTime, Subject, Subscription, takeUntil } from 'rxjs';
+import { BehaviorSubject, debounceTime, Observable, Subject, Subscription, takeUntil } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { UserLoginResponse } from '../../../api/services/user-service/models/user-login-response';
 import { UserApiService } from '../../../api/services/user-service/user-api.service';
@@ -17,14 +17,20 @@ import { StoreService } from '../../../core/services/store/store.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  protected isRegister:Subject<boolean> = new Subject<boolean>()
+  protected isRegister: Subject<boolean> = new Subject<boolean>();
   public get isRewgister$() {
-    return this.isRegister.asObservable()
+    return this.isRegister.asObservable();
   }
-  protected _loginForm!: FormGroup;
-  protected _showLoginError:boolean = false;
 
-  private readonly navigationService:NavigationService  = inject(NavigationService);
+  protected isLogin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  public get isLogin$(): Observable<boolean> {
+    return this.isLogin.asObservable();
+  }
+
+  protected _loginForm!: FormGroup;
+  protected _showLoginError: boolean = false;
+
+  private readonly navigationService: NavigationService = inject(NavigationService);
   private readonly loginFormService: LoginFormService = inject(LoginFormService);
   private readonly _destroy: Subject<boolean> = new Subject<boolean>();
   private readonly userApiService: UserApiService = inject(UserApiService);
@@ -47,12 +53,15 @@ export class LoginComponent implements OnInit, OnDestroy {
   protected signIn(): void {
     this.userApiService.login(this._loginForm).subscribe({
       next: (response: UserLoginResponse) => {
+        this.storeService.setLoggedIn(true);
         this.navigationService.navigateToPollsterMainDashboard();
       },
-      error:(error:HttpErrorResponse) =>{
-        if(error.status === 403 || error.status === 401){
+      error: (error: HttpErrorResponse) => {
+        if (error.status === 403 || error.status === 401) {
           this._showLoginError = true;
-          setTimeout(() => {this._showLoginError = false},7000)
+          setTimeout(() => {
+            this._showLoginError = false;
+          }, 7000);
         }
       }
     });
@@ -68,10 +77,11 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   public ngOnDestroy(): void {
     this._subscription?.unsubscribe();
+    this.isLogin.next(false);
   }
 
   protected navigateToRegister(): void {
-    this.isRegister.next(true)
+    this.isRegister.next(true);
     this.navigationService.navitatoToRegisterComponent();
   }
 }
