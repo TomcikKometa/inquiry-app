c20fa6b4a6740de569bda2086d51f13a9eebbee6
TomcikKometa <tomcik48_84@o2.pl>
2024-10-02 15:29:29 +0200

    develop components

--- src/app/core/interceptors/add-url.interceptor.ts
+++ src/app/core/interceptors/add-url.interceptor.ts
@@ -1,7 +1,32 @@
-import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest, HttpStatusCode } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { ApiUserUrl } from '../../config/api-adress';
+import { inject } from '@angular/core';
+import { NavigationService } from '../services/navigation/navigation.service';
+import { StoreService } from '../services/store/store.service';
+enum AuthInterceptor {
+  AUTH_SCHEME = 'Bearer'
+}
 
-export const addUrlInterceptor: HttpInterceptorFn = (req, next) => {
-  const backendUrl:string = 'http://localhost:3000/api';
-  const backandRequest : HttpRequest<unknown>= req.clone({url:backendUrl + req.url})
-  return next(backandRequest);
-};
+export function addUrlInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const url: string = request.url;
+  const backendUrl: string = 'http://localhost:3000/api';
+  const backandRequest: HttpRequest<unknown> = request.clone({ url: backendUrl + url });
+  const navigationService: NavigationService = inject(NavigationService);
+  const storeService: StoreService = inject(StoreService);
+  const authorizationToken = `${AuthInterceptor.AUTH_SCHEME} ${storeService.getUserToken()}`;
+
+  if (ApiUserUrl.USER_LOGIN_URL === url || ApiUserUrl.USER_CREATE_URL === url) {
+    return next(backandRequest);
+  } else {
+    const req: HttpRequest<unknown> = backandRequest.clone({ setParams: { Authorization: authorizationToken } });
+    return next(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (HttpStatusCode.Forbidden === error.status || HttpStatusCode.Unauthorized=== error.status) {
+          navigationService.navigateToLogin();
+        };
+        return throwError(() => error);
+      })
+    );
+  }
+}
