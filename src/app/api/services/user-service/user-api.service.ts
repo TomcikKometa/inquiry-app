1117b616952049f0a2d390777c86ab7a5239d98f
TomcikKometa <tomcik48_84@o2.pl>
2024-10-02 17:48:38 +0200

    change service

--- src/app/api/services/user-service/user-api.service.ts
+++ src/app/api/services/user-service/user-api.service.ts
@@ -1,26 +1,30 @@
 import { inject, Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { UserLoginResponse } from './models/user-login-response';
 import { HttpClient } from '@angular/common/http';
 import { UserApiMapper } from './user-mapper/user-api-mapper';
 import { UserCreateRequest } from './models/user-login-request';
 import { ApiUserUrl } from '../../../config/api-adress';
+import { StoreService } from '../../../core/services/store/store.service';
+import { NavigationService } from '../../../core/services/navigation/navigation.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
   private readonly http: HttpClient = inject(HttpClient);
+  private readonly storeService: StoreService = inject(StoreService);
+  private readonly navigationService:NavigationService = inject(NavigationService);
 
   public login(loginFormGroup: FormGroup): Observable<UserLoginResponse> {
-    return this.http.post<UserLoginResponse>(
-      ApiUserUrl.USER_LOGIN_URL,
-      UserApiMapper.mapUserLoginRequest(loginFormGroup)
-    );
-  }
+    return this.http
+      .post<UserLoginResponse>(ApiUserUrl.USER_LOGIN_URL, UserApiMapper.mapUserLoginRequest(loginFormGroup))}
 
   public registerUser(registerFormGroup: FormGroup): Observable<UserLoginResponse> {
-   return this.http.post<UserLoginResponse>(ApiUserUrl.USER_CREATE_URL, UserApiMapper.mapRegisterModel(registerFormGroup));
+    return this.http.post<UserLoginResponse>(
+      ApiUserUrl.USER_CREATE_URL,
+      UserApiMapper.mapRegisterModel(registerFormGroup)
+    );
   }
 }
