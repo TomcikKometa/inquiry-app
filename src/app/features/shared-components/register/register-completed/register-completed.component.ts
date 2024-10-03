eac0eef70c8fa28d6740a994a802a0fd890470a6
TomcikKometa <tomcik48_84@o2.pl>
2024-10-03 16:33:52 +0200

    develop components

---
+++ src/app/features/shared-components/register/register-completed/register-completed.component.ts
@@ -0,0 +1,25 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MAT_TABS_CONFIG } from '@angular/material/tabs';
+
+@Component({
+  selector: 'inq-register-completed',
+  standalone: true,
+  providers: [{ provide: MAT_TABS_CONFIG, useValue: { animationDuration: '1ms' } }],
+  imports: [CommonModule],
+
+  templateUrl: './register-completed.component.html',
+  styleUrl: './register-completed.component.css',
+  animations: [
+    trigger('fadeIn', [
+      state('out', style({ opacity: '0' })),
+      state('in', style({ opacity: '1' })),
+      transition('* => *', [animate(2005)])
+    ])
+  ]
+})
+export class RegisterCompletedComponent {
+  @Input() public animationState: string = 'out';
+  @Input() public isRegistred = false;
+}
