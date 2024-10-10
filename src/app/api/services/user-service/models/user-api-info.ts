96457eec195cb114e6f613704817ac3003bc81d4
TomcikKometa <tomcik48_84@o2.pl>
2024-10-10 13:13:45 +0200

    develop inquiry form

---
+++ src/app/api/services/user-service/models/user-api-info.ts
@@ -0,0 +1,5 @@
+export interface UserDetails {
+  role: string;
+  login: string;
+  emia: string;
+}
--- src/app/core/interceptors/add-url.interceptor.ts
+++ src/app/core/interceptors/add-url.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest, HttpStatusCode } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { ApiUserUrl } from '../../config/api-adress';
 import { inject } from '@angular/core';
 import { NavigationService } from '../services/navigation/navigation.service';
@@ -17,10 +17,10 @@ export function addUrlInterceptor(request: HttpRequest<unknown>, next: HttpHandl
   const authorizationToken = `${AuthInterceptor.AUTH_SCHEME} ${storeService.getUserToken()}`;
 
   if (ApiUserUrl.USER_LOGIN_URL === url || ApiUserUrl.USER_CREATE_URL === url) {
-    return next(backandRequest);
+    return next(backandRequest).pipe(tap(response =>  console.log(response)));
   } else {
     const req: HttpRequest<unknown> = backandRequest.clone({headers:request.headers.set('Authorization',authorizationToken)});
-    return next(req).pipe(
+    return next(req).pipe( 
       catchError((error: HttpErrorResponse) => {
         if (HttpStatusCode.Forbidden === error.status || HttpStatusCode.Unauthorized=== error.status) {
           navigationService.navigateToLogin();
